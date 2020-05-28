d9ed6c224acd4ab70afc1da93d2df2614f37e21e
Otsuhachi <nkzm4102@ezweb.ne.jp>
2020-05-28 13:02:25 +0900

    利便性の向上
    - otsutil.funcs, otsutil.exceptions の機能を otsutil のインポートで使用できるようになりました。
    -- `from otsutil.funcs import parg`としていたところを
        `from otsutil import parg`とできるようになりました。

---
+++ otsutil/__init__.py
@@ -0,0 +1,2 @@
+from otsutil.exceptions import InvalidStringError, NotSelectedError, SetPathError
+from otsutil.funcs import choice_dir, choice_file, confirm, create_system_name, deduplicate, deduplicate_file, fline, get_dict_in_value, load_object, parg, save_object, type_input, watch_exec_time
