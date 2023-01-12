ae9a97155382e6c600bd99d8e09155b246543f21
Otsuhachi <nkzm4102@ezweb.ne.jp>
2023-01-13 07:17:21 +0900

    ver 1.0.0
    - バージョン表記を訂正しました。
    - typesモジュールを追加しました。
    - Python 3.7以降で動作するよう修正しました。

--- otsutil/classes.py
+++ otsutil/classes.py
@@ -1,50 +1,60 @@
+"""よく使うクラスを纏めたモジュールです。
+"""
+
+__all__ = (
+    "ObjectSaver",
+    "Timer",
+)
+
+
 import base64
 import pickle
 import time
 
 from datetime import datetime, timedelta
-from pathlib import Path
-from typing import Any, ClassVar, Generator, TypeVar, Union
+from typing import Any, Iterator
 
 from .funcs import setup_path
-
-FLOAT_INT = TypeVar('FLOAT_INT', float, int)
+from .types import hmsValue, pathLike
 
 
 class ObjectSaver:
     """オブジェクトを保存するファイルを扱うクラスです。
+
+    Properties:
+        obj (Any): ファイルに保存されているオブジェクト。
     """
 
-    def __init__(self, file: Union[str, Path]):
-        """オブジェクトを保存するファイルを準備します。
-
-        Args:
-            file (Union[str, Path]): オブジェクトを保存するファイルです。
-        """
+    def __init__(self, file: pathLike) -> None:
         self.__file = setup_path(file)
+        if self.__file.exists():
+            obj = self.load_file()
+        else:
+            obj = None
+        self.__obj = obj
 
     @staticmethod
     def dumps(obj: Any) -> str:
-        """オブジェクトのpickle文字列を取得します。
+        """オブジェクトのpickle文字列を返します。
 
         Args:
-            obj (Any): pickle文字列を取得したいオブジェクトです。
+            obj (Any): pickle文字列を取得したいオブジェクト。
 
         Returns:
-            str: pickle文字列です。
+            str: pickle文字列。
         """
         otb = pickle.dumps(obj, protocol=4)
-        return base64.b64encode(otb).decode('utf-8')
+        return base64.b64encode(otb).decode("utf-8")
 
     @staticmethod
     def loads(pickle_str: str) -> Any:
         """pickle文字列をオブジェクト化します。
 
         Args:
-            pickle_str (str): pickle文字列です。
+            pickle_str (str): pickle文字列。
 
         Returns:
-            Any: 復元されたオブジェクトです。
+            Any: 復元されたオブジェクト。
         """
         stb = base64.b64decode(pickle_str.encode())
         return pickle.loads(stb)
@@ -55,96 +65,103 @@ class ObjectSaver:
         ファイルが存在しなかった場合にはNoneを保存したファイルを生成し、Noneを返します。
 
         Returns:
-            Any: ファイルに保存されていたオブジェクトです。
+            Any: ファイルに保存されていたオブジェクト。
         """
-        if self.__file.exists():
-            with open(self.__file, 'r', encoding='utf-8') as f:
+        file = self.__file
+        if file.exists():
+            with file.open("r", encoding="utf-8") as f:
                 return self.loads(f.read())
         else:
             self.save_file(None)
         return None
 
     def save_file(self, obj: Any) -> bool:
-        """ファイルにobjを保存します。
+        """ファイルにobjを保存し、成否を返します。
 
-        また、loadメソッドで取得できるオブジェクトを更新します。
+        また、obj属性を更新します。
 
         Args:
-            obj (Any): 保存したいオブジェクトです。
+            obj (Any): 保存したいオブジェクト。
 
         Returns:
-            bool: 保存の成否です。
+            bool: 保存の成否。
         """
         try:
             bts = self.dumps(obj)
-            with open(self.__file, 'w', encoding='utf-8') as f:
+            with self.__file.open("w", encoding="utf-8") as f:
                 f.write(bts)
+            self.__obj = obj
             return True
-        except Exception:
+        except:
             return False
 
+    @property
+    def obj(self) -> Any:
+        """ファイルに保存されているオブジェクト。
 
-class OtsuNoneType:
-    """Noneが返るのが正常な場合など、異常なNoneを表す場合に使用するクラスです。
-    """
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __repr__(self) -> str:
-        return 'OtsuNone'
-
-    def __bool__(self) -> bool:
-        return False
+        新規ファイルでインスタンス生成した場合の初期値はNoneになります。
+        """
+        return self.__obj
 
 
 class Timer:
     """指定時間が経過したかを判定したり指定時間秒処理を停止させるタイマーのクラスです。
+
+    Properties:
+        delta (timedelta): インスタンスの基準待機時間です。
+        start_time (datetime): タイマーを開始した時刻です。
+        target_time (datetime): タイマーを終了する時刻です。
     """
-    HOUR_SECONDS: ClassVar[int] = 3600
-    MINUTE_SECONDS: ClassVar[int] = 60
-    MINIMUM_SECONDS: ClassVar[float] = 0.0
-
-    @classmethod
-    def check_minimum_seconds(cls, seconds: FLOAT_INT) -> FLOAT_INT:
-        """指定秒数がTimerクラスで使用可能かを判定し、使用不能な場合には例外を発生させます。
-
-        Args:
-            seconds (FLOAT_INT): 指定秒数。
-
-        Raises:
-            ValueError: Timerクラスで使用不可能な指定秒数が与えられた場合に投げられます。
-
-        Returns:
-            FLOAT_INT: seconds.
-        """
-        if cls.MINIMUM_SECONDS > seconds:
-            msg = f'{cls.MINIMUM_SECONDS}秒未満の秒数を指定することはできません。'
-            raise ValueError(msg)
-        return seconds
 
     def __init__(self, hours: int = 0, minutes: int = 0, seconds: float = 0) -> None:
         """h時間m分s秒を測るタイマーインスタンスを生成します。
 
         Args:
-            hours (int, optional): h時間。
-            minutes (int, optional): m分。
-            seconds (float, optional): s秒。
+            hours (int, optional): h時間 Defaults to 0.
+            minutes (int, optional): m分。 Defaults to 0.
+            seconds (float, optional): s秒。 Defaults to 0.
+
+        Raises:
+            ValueError: マイナス秒になるような時間が指定されている場合に投げられます。
         """
-        seconds += hours * Timer.HOUR_SECONDS
-        seconds += minutes * Timer.MINUTE_SECONDS
-        seconds = Timer.check_minimum_seconds(seconds)
-        self.__seconds = timedelta(seconds=seconds)
+        delta = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        if delta < timedelta(seconds=0):
+            msg = f"0秒未満のTimerインスタンスを生成することはできません。 ({delta.total_seconds():0.2f}秒)"
+            raise ValueError(msg)
+        self.__delta = delta
         self.reset()
+
+    def __bool__(self) -> bool:
+        return self.target_time > datetime.now()
+
+    def __str__(self) -> str:
+        hms = self.calc_hms(self.delta.total_seconds())
+        res = []
+        for i, s in zip(hms, ("時間", "分", "秒")):
+            if i > 0:
+                res.append(f"{i}{s}")
+        return "".join(res) + "のタイマーです。"
+
+    @staticmethod
+    def calc_hms(seconds: float) -> hmsValue:
+        """秒数から時分秒のタプルを返します。
+
+        Args:
+            seconds (float): 秒数。
+
+        Returns:
+            hmsValue: 時分秒のタプル。
+        """
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        h, m = map(int, (h, m))
+        return (h, m, s)
 
     def begin(self, span_seconds: float = 0) -> None:
         """現在から指定時間秒待機します。
 
         Args:
-            span_seconds (float, optional): 待機終了を確認する頻度。
+            span_seconds (float, optional): 待機終了を確認する頻度秒。 Defaults to 0.
         """
         self.reset()
         self.join(span_seconds)
@@ -155,56 +172,51 @@ class Timer:
         Timer.beginと違い、インスタンス生成やTimer.resetからの経過時間に応じて待機時間が減少します。
 
         Args:
-            span_seconds (float, optional): 待機終了を確認する頻度。
+            span_seconds (float, optional): 待機終了を確認する頻度秒。 Defaults to 0.
         """
-        span = Timer.check_minimum_seconds(span_seconds)
+        span = min(0, span_seconds)
         while self:
             time.sleep(span)
 
     def reset(self) -> None:
-        """タイマーの開始時刻をリセットし、終了時刻を更新します。
-        """
+        """タイマーの開始時刻をリセットし、終了時刻を更新します。"""
         self.__start_time = datetime.now()
-        self.__target_time = self.__start_time + self.__seconds
+        self.__target_time = self.start_time + self.delta
 
-    def wiggle_begin(self, span_seconds: float = 0) -> Generator[None, None, None]:
-        """for文で使用することで処理を割り込ませられるTimer.beginです。
+    def wiggle_begin(self) -> Iterator[hmsValue]:
+        """待機時間を確認後に処理を挟むことのできるTimer.beginです。
 
-        Args:
-            span_seconds (float, optional): 処理割込み可能にする頻度秒。
+        for文などと合わせて使うことができます。
 
         Yields:
-            Generator[None, None, None]: None.
+            Iterator[hmsValue]: 指定時刻までの残り時分秒のタプル。
         """
         self.reset()
-        yield from self.wiggle_join(span_seconds)
+        yield from self.wiggle_join()
 
-    def wiggle_join(self, span_seconds: float = 0) -> Generator[None, None, None]:
-        """for文で使用することで処理を割り込ませられるTimer.joinです。
+    def wiggle_join(self) -> Iterator[hmsValue]:
+        """待機時間を確認後に処理を挟むことのできるTimer.joinです。
 
-        Args:
-            span_seconds (float, optional): 処理割込み可能にする頻度秒。
+        for文などと合わせて使うことができます。
 
         Yields:
-            Generator[None, None, None]: None.
+            Iterator[hmsValue]: 指定時刻までの残り時分秒のタプル。
         """
-        span = Timer.check_minimum_seconds(span_seconds)
         while self:
-            yield
-            time.sleep(span)
+            delta = self.target_time - datetime.now()
+            yield self.calc_hms(delta.total_seconds())
 
     @property
-    def seconds(self) -> timedelta:
-        """このタイマーの秒数です。
-        """
-        return self.__seconds
+    def delta(self) -> timedelta:
+        """インスタンスの基準待機時間です。"""
+        return self.__delta
 
     @property
     def start_time(self) -> datetime:
         """タイマーを開始した時刻です。
 
-        この時刻を基準にseconds秒経過したかを判定します。
-        インスタンス生成時、またはTimer.begin, Timer.resetメソッドを呼び出した場合にこの属性が変更されます。
+        この時刻を基準にdelta秒経過したかを判定します。
+        インスタンス生成時、またはTimer.begin, Timer.resetメソッドを呼び出した場合にこの属性が更新されます。
         """
         return self.__start_time
 
@@ -212,22 +224,6 @@ class Timer:
     def target_time(self) -> datetime:
         """タイマーを終了する時刻です。
 
-        インスタンス生成時、またはTimer.begin, Timer.resetメソッドを呼び出した場合にこの属性が変更されます。
+        インスタンス生成時、またはTimer.begin, Timer.resetメソッドを呼び出した場合にこの属性が更新されます。
         """
         return self.__target_time
-
-    def __str__(self) -> str:
-        seconds = self.__seconds.total_seconds()
-        h, seconds = divmod(seconds, Timer.HOUR_SECONDS)
-        m, seconds = divmod(seconds, Timer.MINUTE_SECONDS)
-        res = []
-        for i, s in zip(map(int, (h, m, seconds)), ('時間', '分', '秒')):
-            if i > 0:
-                res.append(f'{i}{s}')
-        return ''.join(res) + 'のタイマーです。'
-
-    def __bool__(self) -> bool:
-        return self.__target_time > datetime.now()
-
-
-OtsuNone = OtsuNoneType()
--- otsutil/funcs.py
+++ otsutil/funcs.py
@@ -1,167 +1,265 @@
+from __future__ import annotations
+
+"""よく使う関数を纏めたモジュールです。
+"""
+
+__all__ = (
+    "deduplicate",
+    "load_json",
+    "read_lines",
+    "save_json",
+    "setup_path",
+    "str_to_path",
+    "write_lines",
+)
+
+
 import json
 
+from collections import deque
 from pathlib import Path
-from typing import Generator, Iterable, Union, overload
+from typing import (
+    Deque,
+    Iterable,
+    Iterator,
+    List,
+    Sequence,
+    Tuple,
+    Union,
+    overload,
+)
+
+from .types import T, pathLike
 
 
 @overload
-def deduplicate(values: list) -> list:
+def deduplicate(values: Deque[T]) -> Deque[T]:
+    """デックから重複を取り除きます。
+
+    この関数はset(values)で順番を破壊したくない場合などに使用します。
+
+    Args:
+        values (Deque[T]): 重複を取り除きたいデック。
+
+    Returns:
+        Deque[T]: 重複を除去したデック。
+    """
+    ...
+
+
+@overload
+def deduplicate(values: List[T]) -> List[T]:
     """リストから重複を取り除きます。
 
     この関数はset(values)で順番を破壊したくない場合などに使用します。
 
     Args:
-        values (list): 対象のリストです。
+        values (List[T, ...]): 重複を取り除きたいリスト。
 
     Returns:
-        list: 重複を除去したリストです。
+        List[T, ...]: 重複を除去したリスト。
     """
     ...
 
 
 @overload
-def deduplicate(values: tuple) -> tuple:
+def deduplicate(values: Tuple[T]) -> Tuple[T]:
+    """tupleから重複を取り除きます。
+
+    この関数はset(values)で順番を破壊したくない場合などに使用します。
+
+    Args:
+        values (Tuple[T]): 重複を取り除きたいtuple。
+
+    Returns:
+        Tuple[T]: 重複を除去したtuple。
+    """
+    ...
+
+
+@overload
+def deduplicate(values: Tuple[T, ...]) -> Tuple[T, ...]:
     """タプルから重複を取り除きます。
 
     この関数はset(values)で順番を破壊したくない場合などに使用します。
 
     Args:
-        values (tuple): 対象のタプルです。
+        values (Tuple[T, ...]): 重複を取り除きたいタプル。
 
     Returns:
-        tuple: 重複を除去したタプルです。
+        Tuple[T, ...]: 重複を除去したタプル。
     """
     ...
 
 
-def deduplicate(values: Union[list, tuple]) -> Union[list, tuple]:
-    f = type(values)
-    if f not in (list, tuple):
-        msg = f'(list, tuple)いずれかの型である必要があります。'
-        raise TypeError(msg)
+def deduplicate(values: Sequence[T]) -> Sequence[T]:
+    """シーケンスから重複を取り除きます。
+
+    この関数はset(values)で順番を破壊したくない場合などに使用します。
+    また、シーケンスがデック, リスト, タプルのいずれかの場合はシーケンスの型も保持します。
+    それ以外の場合はList[T]になります。
+
+    Args:
+        values (Sequence[T]): 重複を取り除きたいシーケンス。
+
+    Returns:
+        Sequence[T]: 重複を除去したシーケンス。
+    """
     res = sorted(set(values), key=values.index)
-    return f(res)
+    if type(values) is tuple:
+        res = tuple(res)
+    elif type(values) is deque:
+        res = deque(res)
+    return res
 
 
-def load_json(file: Union[str, Path], encoding: str = 'utf-8', **kwargs) -> Union[dict, list]:
+def load_json(file: pathLike, encoding: str = "utf-8", **kwargs) -> Union[dict, list]:
     """json形式のファイルを読み込み、jsonデータを返します。
 
     キーワード引数にはjson.loadで使用できる引数を与えることができます。
 
     Args:
-        file (Path): json形式のファイルです。
-        encoding (str, optional): ファイルのエンコードです。指定しない場合utf-8で扱います。
+        file (Path): json形式のファイル。
+        encoding (str, optional): ファイルのエンコード。指定しない場合utf-8で扱います。
 
     Raises:
         FileNotFoundError: ファイルが存在しないかフォルダの場合に投げられます。
 
     Returns:
-        Union[dict, list]: 読み込んだJSONです。
+        Union[dict, list]: jsonデータ。
     """
-    file = setup_path(file)
+    file = str_to_path(file)
     if not file.is_file():
-        msg = f'{file}は存在しないかファイルではありません。'
+        msg = f"{file}は存在しないかファイルではありません。"
         raise FileNotFoundError(msg)
-    with open(file, 'r', encoding=encoding) as f:
-        kwargs['fp'] = f
+    with file.open("r", encoding=encoding) as f:
+        kwargs["fp"] = f
         return json.load(**kwargs)
 
 
-def read_lines(file: Union[str, Path], ignore_blankline: bool = False, **kwargs) -> Generator[str, None, None]:
-    """ファイルを読み込み、1行ずつ返すジェネレータです。
+def read_lines(file: pathLike, ignore_blank_line: bool = False, encoding: str = "utf-8", **kwargs) -> Iterator[str]:
+    """ファイルを読み込み、1行ずつ返すイテレータを生成します。
 
     行右端の改行を自動で除去します。
-    
+
     キーワード引数にはopenで使用できる引数を与えることができますが、modeはrで固定されます。
-    encodingは指定しなかった場合utf-8になります。
 
     Args:
-        file (Union[str, Path]): 読み込むファイルです。
-        ignore_blankline (bool, optional): line.strip()したときに空文字になる行を無視します。
+        file (pathLike): 読み込むファイル。
+        ignore_blank_line (bool, optional): line.strip()したときに空文字になる行を無視します。 Defaults to False.
+        encoding (str, optional): 読み込むファイルのエンコード。 Defaults to "utf-8".
+
+    Raises:
+        FileNotFoundError: ファイルが存在しないかフォルダの場合に投げられます。
 
     Yields:
-        Generator[str, None, None]: ファイルの行です。
+        Iterator[str]: ファイルの各行を返すイテレータ。
     """
-    file = setup_path(file)
-    kwargs['file'] = file
-    kwargs['mode'] = 'r'
-    if kwargs.get('encoding') is None:
-        kwargs['encoding'] = 'utf-8'
-    with open(**kwargs) as f:
-        gen = map(lambda x: x.rstrip('\n'), f)
-        if ignore_blankline:
+    file = str_to_path(file)
+    if not file.is_file():
+        msg = f"{file}は存在しないかファイルではありません。"
+        raise FileNotFoundError(msg)
+    kwargs["encoding"] = encoding
+    if "file" in kwargs:
+        del kwargs["file"]
+    kwargs["mode"] = "r"
+    with file.open(**kwargs) as f:
+        gen = map(lambda x: x.rstrip("\n"), f)
+        if ignore_blank_line:
             gen = filter(lambda x: x.strip(), gen)
         for line in gen:
             yield line
 
 
-def save_json(file: Union[str, Path], data: Union[dict, list], encoding: str = 'utf-8', **kwargs) -> None:
+def save_json(
+    file: pathLike,
+    data: Union[dict, list],
+    encoding: str = "utf-8",
+    ensure_ascii: bool = False,
+    indent: Union[int, str, None] = 4,
+    sort_keys: bool = True,
+    **kwargs,
+) -> None:
     """指定したファイルにデータをjson形式で書き出します。
 
     キーワード引数にはjson.dumpで使用できる引数を与えることができます。
 
     Args:
-        file (Path): 出力先のファイルです。
-        data (Union[list,dict]): 出力するデータです。
-        encoding (str): ファイルのエンコードです。指定しない場合utf-8で扱います。
+        file (pathLike): 出力先ファイル。
+        data (Union[dict, list]): 出力するデータ。
+        encoding (str, optional): 出力するファイルのエンコード。 Defaults to "utf-8".
+        ensure_ascii (bool, optional): json.dumpsのensure_ascii引数。 Defaults to False.
+        indent (Union[int, str, None], optional): json.dumpsのindent引数 Defaults to 4.
+        sort_keys (bool, optional): json.dumpsのsort_keys引数 Defaults to True.
     """
     file = setup_path(file)
-    with open(file, 'w', encoding=encoding) as f:
-        kwargs['fp'] = f
-        kwargs['obj'] = data
+    with file.open("w", encoding=encoding) as f:
+        kwargs["fp"] = f
+        kwargs["obj"] = data
+        kwargs["ensure_ascii"] = ensure_ascii
+        kwargs["indent"] = indent
+        kwargs["sort_keys"] = sort_keys
         json.dump(**kwargs)
 
 
-def setup_path(path: Union[str, Path], is_dir: bool = False) -> Path:
+def setup_path(path: pathLike, is_dir: bool = False) -> Path:
     """親ディレクトリの存在を確認、生成、保証しパスを返します。
 
     is_dirがTrueの場合にはpathを生成します。
 
     Args:
-        path (Union[str, Path]): 使用したいパスです。
-        is_dir (bool, optional): パスがディレクトリかどうかです。
+        path (Union[str, Path]): 使用したいパス。
+        is_dir (bool, optional): パスがディレクトリかどうか。
 
     Returns:
-        Path: 使用可能なパスです。
+        Path: 使用可能なパス。
     """
-    if not isinstance(path, Path):
-        path = Path(path)
+    path = str_to_path(path)
     if is_dir:
         p = path
     else:
         p = path.parent
-
     if not p.exists():
         p.mkdir(parents=True)
     return path
 
 
-def write_lines(file: Union[str, Path], lines: Iterable, add_blank_line: bool = False, **kwargs) -> None:
-    """ファイルにlinesを１行ずつ書き出します。
-
-    各行の改行、空白記号はそのまま保持されます。
-    そのためlen(lines)とfileに書き出された行数は一致しない場合があります。
-
-    キーワード引数にはopenで使用できる引数を与えることができます。
-    modeは指定されていない場合はwになります。
-    またmodeが適正かどうかはopen文でチェックされるので、rなどを与えると例外が発生します。
-    encodingは指定しなかった場合utf-8になります。
+def str_to_path(path: pathLike) -> Path:
+    """文字列をパスに変換します。
 
     Args:
-        file (Union[str, Path]): 書き出し先のファイルです。
-        lines (Iterable): 書き出す行です。
-        add_blank_line (bool, optional): ファイル末尾に空行を追加するかどうかです。
+        path (pathLike): パスにしたい文字列。またはパス。
+
+    Returns:
+        Path: パス。
+    """
+    if isinstance(path, Path):
+        return path
+    return Path(path)
+
+
+def write_lines(file: pathLike, lines: Iterable, add_blank_line: bool = False, encoding: str = "utf-8", **kwargs) -> None:
+    """ファイルにlinesを1行ずつ書き出します。
+
+    キーワード引数にはPath().openで使用できる引数を与えることができます。
+
+    Args:
+        file (pathLike): 出力先ファイル。
+        lines (Iterable): 出力する行データ。
+        add_blank_line (bool, optional): ファイル末尾が空白行になるようにするか。 Defaults to False.
+        encoding (str, optional): 出力するファイルのエンコード。 Defaults to "utf-8".
     """
     file = setup_path(file)
-    kwargs['file'] = file
-    if kwargs.get('mode') is None:
-        kwargs['mode'] = 'w'
-    if kwargs.get('encoding') is None:
-        kwargs['encoding'] = 'utf-8'
-    with open(**kwargs) as f:
+    kwargs["encoding"] = encoding
+    kwargs["mode"] = "w"
+    if "file" in kwargs:
+        del kwargs["file"]
+    with file.open(**kwargs) as f:
+        last_line = ""
         for i, line in enumerate(map(str, lines)):
+            last_line = line
             if i:
-                line = f'\n{line}'
+                line = f"\n{line}"
             f.write(line)
-        if add_blank_line:
-            f.write('\n')
+        if add_blank_line and last_line.strip():
+            f.write("\n")
